6ec7353de81ed4752724403922f6d9b45d3b1293
n13PAG <74218503+n13PAG@users.noreply.github.com>
2023-05-19 18:44:02 -0700

    Update Coordinator
    Coordinator handles all externals calls for entity creation, modification and deletion.

--- Core/Coordinator.h
+++ Core/Coordinator.h
@@ -1,21 +1,39 @@
 #pragma once
 
 #include "MECS.h"
-//
-//#include "EntityManager.h"
-//
-//#include "ComponentManager.h"
-//#include "..\Components\Position.h"
-//
-//#include <array>
-//#include <vector>
-//#include <memory>
+
+#include "../Core/component_manager.h"
+#include "../Core/entity_manager.h"
 
 namespace MECS {
 	class Coordinator {
-		//std::array<ComponentManager<Component>*, COMPONENT_TYPE_COUNT> mComponentManagers{
-		//	new ComponentManager<Position>()
-		//}
-		
+	private:
+		ComponentManager component_manager;
+		EntityManager entity_manager;
+	public:
+		Entity CreateEntity() {
+			return entity_manager.CreateEntity();
+		}
+
+		void DestroyEntity(Entity entity_id) {
+			component_manager.RemoveAllComponents(entity_id, entity_manager.GetSignature(entity_id));
+			entity_manager.DestroyEntity(entity_id);
+		}
+
+		template<typename T>
+		void AddComponent(Entity entity_id) {
+			entity_manager.AddSignatureBit<T>(entity_id, component_manager.AddComponent<T>(entity_id));
+		}
+
+		template<typename T>
+		void RemoveComponent(Entity entity_id) {
+			entity_manager.RemoveSignatureBit<T>(entity_id, component_manager.GetComponentType<T>());
+			component_manager.RemoveComponent<T>(entity_id);
+		}
+
+		template<typename T>
+		T& GetComponent(Entity entity_id) {
+			return component_manager.GetComponent<T>(entity_id);
+		}
 	};
 }
