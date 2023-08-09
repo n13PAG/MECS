68d98bcea55c05dbed7e27e531f8f350d042bd0d
n13PAG <74218503+n13PAG@users.noreply.github.com>
2023-08-09 14:53:03 -0700

    Update component list and move component type to MECS file

--- Core/MECS.h
+++ Core/MECS.h
@@ -8,27 +8,30 @@ namespace MECS {
 
 	// Entity Constants
 	using Entity = std::uint32_t;
-	static const Entity kMaxEntities = 400;
+	static const Entity kMaxEntities = 200;
 
 	// Component IDs
 	// All new components must be included here
 	const enum ComponentTypes {
-		kPosition = 0,
-		kRotation = 1,
-		kRect = 2,
-		kCirc = 3,
-		kOutline = 4,
-		kRenderable = 5,
-		kText = 6,
-		kCustomText = 7,
-		kVelocity = 8,
-		kSprite = 9,
-		kAudio = 10,
-		kCircleCollider = 11,
-		kCompTypeNum = 12
+		kTransform,
+		kRect,
+		kCirc,
+		kOutline,
+		kRenderable,
+		kText,
+		kCustomText,
+		kVelocity,
+		kSprite,
+		kAudio,
+		kCircleCollider,
+		kAdvParticle2D_Rect,
+		kUI_Button,
+		kCompTypeNum
 	};
 
 	static const uint8_t kComponentTypeCount = kCompTypeNum;
 
 	using Signature = std::bitset<kComponentTypeCount>;
+	using ComponentType = Signature;
+
 }
